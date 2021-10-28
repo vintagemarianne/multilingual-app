745afc34deb0c6a4c02c9a0abb6d63b27ec2f4fe
Saleh Yusefnejad <msynk@outlook.com>
2021-10-28 17:05:57 +0330

    add CultureService

---
+++ src/CultureService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace MultilingualApp
+{
+    public class CultureService
+    {
+        private IJSRuntime _jsRuntime;
+        private HttpClient _httpClient;
+
+        public CultureService(IJSRuntime js, HttpClient http)
+        {
+            _jsRuntime = js;
+            _httpClient = http;
+        }
+
+        public async Task InitAsync()
+        {
+            var cultureName = await _jsRuntime.InvokeAsync<string>("window.localStorage.getItem", "my-blazor-culture");
+
+            if (cultureName == null)
+            {
+                cultureName = await _httpClient.GetStringAsync("http://getcurrentculture...");
+            }
+            await SetCurrentCultureAsync(cultureName);
+        }
+
+        public async Task SetCurrentCultureAsync(string name)
+        {
+            var culture = new CultureInfo(name);
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            await _jsRuntime.InvokeVoidAsync("window.localStorage.setItem", "my-blazor-culture", "en-US");
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -9,28 +9,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-//await builder.Build().RunAsync();
+builder.Services.AddScoped<CultureService>();
 builder.Services.AddLocalization();
 
 var host = builder.Build();
 
-CultureInfo culture;
-var js = host.Services.GetRequiredService<IJSRuntime>();
-
-var cultureName = await js.InvokeAsync<string>("window.localStorage.getItem", "my-blazor-culture");
-
-if (cultureName != null)
-{
-    culture = new CultureInfo(cultureName);
-}
-else
-{
-    culture = new CultureInfo("en-US");
-    await js.InvokeVoidAsync("window.localStorage.setItem", "my-blazor-culture", "en-US");
-}
-
-CultureInfo.DefaultThreadCurrentCulture = culture;
-CultureInfo.DefaultThreadCurrentUICulture = culture;
+var cultureService = host.Services.GetRequiredService<CultureService>();
+await cultureService.InitAsync();
 
 await host.RunAsync();
